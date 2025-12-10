03c750983a0aafc3be2a1ec9ae8ea913c8a5b2ba
Adam ONeill <adamaoneill@gmail.com>
2025-12-10 22:59:58 +0000

    background star complete, hero section redesigned, svg images for skills removed and to be replaced with proper logos soon

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import {Component, inject, signal} from '@angular/core';
+import {AfterViewInit, Component, Inject, inject, PLATFORM_ID, signal} from '@angular/core';
 import {Project, Skill} from '../models/project.model';
 import {DataService} from '../services/data.service';
 import {HeaderComponent} from '../components/header/header.component';
 import {SkillBadgeComponent} from '../components/skill-badge/skill-badge.component';
 import {ProjectCardComponent} from '../components/project-card/project-card.component';
 import {ProjectModalComponent} from '../components/project-modal/project-modal.component';
-import {DatePipe} from '@angular/common';
+import {DatePipe, isPlatformBrowser} from '@angular/common';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,132 @@ import {DatePipe} from '@angular/common';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('portfolio');
+export class App implements AfterViewInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    // 1. Check if we are in the browser
+    if (isPlatformBrowser(this.platformId)) {
+
+      // 2. Wrap in timeout to ensure the DOM element #particles-js exists
+      setTimeout(() => {
+        this.initParticles();
+      }, 0);
+    }
+  }
+
+  initParticles() {
+    particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 150,
+          "density": {
+            "enable": true,
+            "value_area": 1500
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 3,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 2,
+          "random": true,
+          "anim": {
+            "enable": true,
+            "speed": 1,
+            "size_min": 1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 100,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 0.2,
+          "direction": "top",
+          "random": true,
+          "straight": true,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 1183.723797780518,
+            "rotateY": 1657.213316892725
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false,
+            "mode": "grab"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 85.26836695850781,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 79.92007992007991,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+    });
+  }
 
   private dataService = inject(DataService);
 
